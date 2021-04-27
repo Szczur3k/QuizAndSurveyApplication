73905ea5b3fba385a003052c440ea7da374d5d88
Szczur3k <Mateuszlukaszczyk13@gmail.com>
2021-04-27 19:43:11 +0200

    Initial commit

---
+++ src/main/java/pl/MateuszLukaszczyk/QuizAndSurveyApplication/QuizAndSurveyApplication.java
@@ -0,0 +1,13 @@
+package pl.MateuszLukaszczyk.QuizAndSurveyApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAndSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAndSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/MateuszLukaszczyk/QuizAndSurveyApplication/QuizAndSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.MateuszLukaszczyk.QuizAndSurveyApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAndSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
